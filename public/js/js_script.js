a908372c0b6766fddc3eb38f6c3e4ab89d44aff9
eriklundman <erik.lundman@bellkom.se>
2019-11-28 11:26:56 +0100

    Klar med js fiddle

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -12,3 +12,24 @@ function MenuItem(name, kCal, price, gluten, lactose) {
 let burger1 = new MenuItem("Tower Burger", "900", "80", true, true);
 console.log(burger1.gluten);
 console.log(burger1.info());
+
+document.getElementById("hej");
+var par = document.createElement('p');
+
+
+
+methods: {
+    MenuItem: function(name, kCal, price, gluten, lactose) {
+        this.name = name;
+        this.kCal = kCal;
+        this.price = price;
+        this.gluten = gluten;
+        this.lactose = lactose;
+        this.info = function() {
+            return this.name + " contains " + this.kCal + " kCal";
+        };
+    },
+
+    burger1: new MenuItem("Tower Burger", "900", "80", true, true),
+
+},
